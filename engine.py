b0ed267c98fe3e8c8ccbe0210e97dfcf82a968e5
Rudy <c01010010@hotmail.com>
2020-07-01 00:49:18 -0400

    implements ranking

--- engine.py
+++ engine.py
@@ -37,7 +37,7 @@ class FeatureDB(object):
 
     def export_features(self, output_db_name: str, method: str):
         """
-        Exports features in a pickle object.
+        Exports features to a pickle object.
 
         :param output_db_name:
             Name of output database with features.
@@ -67,11 +67,22 @@ class FeatureDB(object):
 
         print("Ready!")
 
+    def __len__(self):
+        if self.features:
+            return len(self.features)
+        elif self.images:
+            return len(self.images)
+        return None
+
 
 class Ranker(object):
 
     def __init__(self, feature_db: FeatureDB):
         self.feature_db: FeatureDB = feature_db
+
+    @staticmethod
+    def extract_class(s: str):
+        return int(s.split(".")[0][:3])
 
     def query(self, image: np.array, similarity_fn: DistanceMeasure) -> List[np.array]:
 
@@ -85,13 +96,36 @@ class Ranker(object):
 
         return sorted(similarities, key=lambda x: x[1], reverse=True)
 
+    def query_rank(self, image: np.array, image_label: str, similarity_fn: DistanceMeasure, top_k: int = 10):
+
+        query_class = self.extract_class(image_label)
+        query_results = self.query(image, similarity_fn)
+        top_query_results = query_results[:top_k]
+        rank = 0
+        total_query_in_class = 0
+        for idx, (label, score) in enumerate(top_query_results):
+            img_class = self.extract_class(label)
+            if img_class == query_class:
+                rank += idx + 1
+                total_query_in_class += 1
+
+        rank /= total_query_in_class
+        return top_query_results, rank, total_query_in_class
+
+    def query_normalized_rank(self, image: np.array, image_label: str, similarity_fn: DistanceMeasure, top_k: int = 10):
+        top_query_results, rank, total_query_in_class = self.query_rank(image, image_label, similarity_fn, top_k)
+        norm_rank = (rank - (total_query_in_class + 1) / 2) / len(self.feature_db)
+        return top_query_results, norm_rank
+
 
 if __name__ == '__main__':
     img_path = 'data/jpg'
     db_path = 'data/dbs/first_db'
-    img = cv.cvtColor(cv.imread('data/jpg/100101.jpg', cv.IMREAD_COLOR), cv.COLOR_BGR2RGB)
+    img_label = '100000.jpg'
+
+    img = cv.cvtColor(cv.imread(f'data/jpg/{img_label}', cv.IMREAD_COLOR), cv.COLOR_BGR2RGB)
     f = FeatureDB.load_feature_db(db_path)
     r = Ranker(f)
     d = CosineDistance()
-    results = r.query(img, d)
+    results = r.query_normalized_rank(img, img_label, d)
 
